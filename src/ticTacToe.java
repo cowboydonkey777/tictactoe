d733669a8d41215a1ffafab25992eb8f319444a9
cowboydonkey777 <yggdrasil789@gmail.com>
2019-03-29 12:27:29 -0700

    gonna implement grid into the game

--- src/ticTacToe.java
+++ src/ticTacToe.java
@@ -4,21 +4,49 @@ import java.awt.event.*;
 import javax.swing.*;
 import javax.swing.JOptionPane.*;
 
-public class ticTacToe extends JPanel
+public class ticTacToe extends GridGame
 {
   enum gameState { Start, Running, Gameover }
   private gameState GAMESTATE = gameState.Start;
+  //private String nInput = JOptionPane.showInputDialog("Enter size of board");
+  private int n = 7;//Integer.parseInt(nInput);
 
-  static int n = 5;
-  int[][] tiles = new int[n][n];
+  //Grid[][] tiles = new Grid[n][n];
+  private int[][] tiles = new int[n][n];
   // 0: empty, 1: X, 2: O
-  int turn = 7;
+  private int turn = 7;
 
   private Color RED = new Color(0xFF0000);
   private Color BLUE = new Color(0x0000FF);
   private Color gridColor = new Color(0x8F8F8F);
-  private Color TILE = new Color(0xD3D3D3);
 
+  public int getStatus()
+  {
+    return 0;
+  }
+  public Boolean placeMark(int row, int col, int player)
+  {
+    return true;
+  };
+
+  public int placeMark(int player)
+  {
+    if (turn == 7) turn = 10;
+    else turn = 7;
+    return player;
+  };
+
+
+  public void restart()
+  {
+    if(GAMESTATE == gameState.Start)
+    {
+      tiles = new int[n][n];
+        for(int i = 0; i < n; i++){ for(int j = 0; j < n; j++)
+        { tiles[i][j] = 0;}}
+      GAMESTATE = gameState.Running;
+    }
+  };
 
   public ticTacToe()
   {
@@ -26,6 +54,8 @@ public class ticTacToe extends JPanel
     setPreferredSize(new Dimension((n*75)+14, (n*75)+14));
     setBackground(new Color(0xc8a2c8));
     setFocusable(true);
+
+    //  ---------------------------------------------Mouse Listener-----------------------------------------
 
     addMouseListener(new MouseAdapter()
     {
@@ -44,9 +74,7 @@ public class ticTacToe extends JPanel
               if(ypos > 10+j*75 && ypos < (10+70)+(j*75))
               {
               if(tiles[i][j] == 0) {
-                  tiles[i][j] = turn;
-                  if (turn == 7) turn = 10;
-                  else turn = 7;
+                  tiles[i][j] = placeMark(turn);
               }
               }
             }
@@ -67,6 +95,8 @@ public class ticTacToe extends JPanel
       }
     });
 
+    // --------------------------------------------------Key Listener------------------------------------------
+
     addKeyListener(new KeyAdapter()
     {
       @Override
@@ -75,13 +105,16 @@ public class ticTacToe extends JPanel
         switch (ke.getKeyCode())
         {
           case KeyEvent.VK_ENTER:
-            startGame();
+            restart();
             break;
         }
         repaint();
       }
     });
+
   }
+
+  // -------------------------------------------------------paintComponent-----------------------------
 
   @Override
   public void paintComponent(Graphics gg)
@@ -89,22 +122,21 @@ public class ticTacToe extends JPanel
     super.paintComponent(gg);
     Graphics2D g = (Graphics2D) gg;
 
-    drawGrid(g);
+    printGrid(g);
   }
 
-  void startGame() {
-  if(GAMESTATE == gameState.Start)
-  {
-    tiles = new int[n][n];
-    if(GAMESTATE != gameState.Running){
-      for(int i = 0; i < n; i++){ for(int j = 0; j < n; j++)
-      { tiles[i][j] = 0;}}}
-    GAMESTATE = gameState.Running;
-  }
-}
+  // ------------------------------------------------------startGame-------------------------------------
 
-  void drawGrid(Graphics2D g)
+  //private void restart() {
+  //}
+
+  // ------------------------------------------------------drawGame--------------------------------------
+
+  public void printGrid(Graphics2D g)
   {
+    Color TILE;
+    if(GAMESTATE == gameState.Start) TILE = new Color(0x434343);
+    else TILE = new Color(0xD3D3D3);
     g.setColor(gridColor);
     g.fillRect(7, 7, n*75,n*75);
     g.setStroke(new BasicStroke(1));
@@ -142,11 +174,13 @@ public class ticTacToe extends JPanel
       }
     }
     repaint();
-  }
+  };
+  //private void drawGrid(Graphics2D g)
+  //{ }
 
-  endValue end()
+  private endValue end()
   {
-    int L = 5;
+    int L = 4;
     if(L > n) L = n;
 
     int diagDownRight = 0;
@@ -163,32 +197,24 @@ public class ticTacToe extends JPanel
       for(int k = 0; k < L; k++)
       {
         hori += tiles[k+i][j];
-        vert += tiles[j][k+i];// + tiles[i][j+1] + tiles[i][j+2];
+        vert += tiles[j][k+i];
         if((j+k) <= (n-1)){
           diagDownRight += tiles[k+i][k+j];
           diagDownLeft += tiles[(n-1)-(k+i)][k+j];
-          //System.out.print("stuff: " + (k+i) + " " + (k+j) + '\n');
         }
-        //diagDownLeft += tiles[(n - j) - (k+1)][i+k];
       }
-      /*for(int k = 0; k < (n-L+1); k++)
-      {
-        diagDownRight += tiles[i][k];
-      }*/
-      System.out.print("hori: " + hori + " vert: " + vert + " diagDR: " + diagDownRight + " diagDL: " + diagDownLeft + '\n');
-      //diagDownLeft += tiles[(n - j) - 1][i] + tiles[(n - j) - 2][i + 1] + tiles[(n - j) - 3][i + 2];
+      for(int k = 0; k < n; k++) if(tiles[j][k] == 0) somethingExists++;
       if(vert == 7*L || hori == 7*L || vert == 10*L || hori == 10*L) break;
       else if(diagDownRight == 7*L || diagDownRight == 10*L  || diagDownLeft == 7*L|| diagDownLeft == 10*L) break;
-      somethingExists++;
+      else if(somethingExists == n*n) break;
     }
       if(diagDownRight == 7*L || diagDownLeft == 7*L || vert == 7*L || hori == 7*L)
       {check.winner = true; check.team = 1; break;} // x wins
       else if(diagDownRight == 10*L || diagDownLeft == 10*L || vert == 10*L || hori == 10*L)
       {check.winner = true; check.team = 2; break;} // o wins
-      else if(i == n-1 && somethingExists == n*n)
-      {check.winner = true; check.team = 0; break;} // no one wins
+      else if(somethingExists == 0)
+      {check.winner = true; break;} // no one wins
     }
-    System.out.println('\n');
     return check;
   }
 
@@ -196,14 +222,14 @@ public class ticTacToe extends JPanel
   {
     SwingUtilities.invokeLater(() ->
     {
-      JFrame f = new JFrame();
-      f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-      f.setTitle("Tic-Tac-Toe");
-      f.setResizable(false);
-      f.add(new ticTacToe(), BorderLayout.CENTER);
-      f.pack();
-      f.setLocationRelativeTo(null);
-      f.setVisible(true);
+      JFrame myJFrame = new JFrame();
+      myJFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+      myJFrame.setTitle("Tic-Tac-Toe");
+      myJFrame.setResizable(false);
+      myJFrame.add(new ticTacToe(), BorderLayout.CENTER);
+      myJFrame.pack();
+      myJFrame.setLocationRelativeTo(null);
+      myJFrame.setVisible(true);
     });
   }
 }
